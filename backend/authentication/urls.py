f34188e4645765c99b0075725ed8ca5db866a580
najibthapa1 <thapanajib@gmail.com>
2026-01-29 00:21:44 +0545

    Add profile, study goals and activity URLs

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,15 @@ urlpatterns = [
     path('verify-email/', views.verify_email, name='verify_email'),
     path('resend-verification/', views.resend_verification, name='resend_verification'),
     path('login/', views.login, name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', views.get_profile, name='get_profile'),
+    path('profile/update/', views.update_profile, name='update_profile'),
+    path('profile/upload-picture/', views.upload_profile_picture, name='upload_profile_picture'),    
+    path('profile/stats/', views.get_user_stats, name='get_user_stats'),
+    path('profile/activity/', views.get_activity_timeline, name='get_activity_timeline'),    
+    path('study-goals/', views.get_study_goals, name='get_study_goals'),
+    path('study-goals/create/', views.create_study_goal, name='create_study_goal'),
+    path('study-goals/<uuid:goal_id>/update/', views.update_study_goal, name='update_study_goal'),
+    path('study-goals/<uuid:goal_id>/delete/', views.delete_study_goal, name='delete_study_goal'),
+    path('study-goals/<uuid:goal_id>/toggle/', views.toggle_study_goal, name='toggle_study_goal'),
 ]
