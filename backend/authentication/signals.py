ebbc9b6196f578e765639edfc6f26803d93bf663
najibthapa1 <thapanajib@gmail.com>
2026-02-03 19:46:20 +0545

    Create signals for auto guild creation

---
+++ backend/authentication/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile, Guild, AdminNotification
+
+@receiver(post_save, sender=Profile)
+def create_guild_and_notification(sender, instance, created, **kwargs):
+    """Auto-create guild when new profile is created and send admin notification"""
+    if created:
+        # Create guild if doesn't exist
+        guild, guild_created = Guild.objects.get_or_create(
+            name=instance.university_name,
+            defaults={'description': f'Official guild for {instance.university_name} students'}
+        )
+        
+        # Update member count
+        guild.update_member_count()
+        
+        # Create admin notification
+        AdminNotification.objects.create(
+            notification_type='user',
+            title='New User Registration',
+            description=f'{instance.full_name} just signed up from {instance.university_name}'
+        )
