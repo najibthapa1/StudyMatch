12b4d4af87b2c9f966adb34e94da066feac5601e
najibthapa1 <thapanajib@gmail.com>
2026-03-17 14:41:09 +0545

    added network server

--- frontend/studymatch_frontend/vite.config.js
+++ frontend/studymatch_frontend/vite.config.js
@@ -3,15 +3,17 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Get __dirname equivalent in ES modules
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173
   },
 })
