cea7543d86eda53888adf97b99969e45619c6536
najibthapa1 <thapanajib@gmail.com>
2026-03-18 07:28:03 +0545

    Changes in ip address

--- backend/studymatch_backend/settings.py
+++ backend/studymatch_backend/settings.py
@@ -191,6 +191,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",  
     "http://127.0.0.1:5173",
+    "http://100.64.218.39:5173",  
+    "http://100.64.218.39:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
