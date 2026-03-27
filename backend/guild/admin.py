ea757dc2cf79bf9fb8f9903405f789682f1c5317
najibthapa1 <thapanajib@gmail.com>
2026-03-27 23:53:49 +0545

    guild admin

--- backend/guild/admin.py
+++ backend/guild/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Guild, Event, EventParticipant
+from .models import Guild, Event, EventParticipant, EventPhoto
 
 admin.site.register(Guild)
 admin.site.register(Event)
 admin.site.register(EventParticipant)
+admin.site.register(EventPhoto)
